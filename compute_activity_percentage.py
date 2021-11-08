238a0743c92d9845cc83ac7cee58b1b7cffe3bdc
Romain <romain.caneill@yahoo.fr>
2021-11-08 11:54:41 +0100

    small change

--- compute_activity_percentage.py
+++ compute_activity_percentage.py
@@ -5,25 +5,12 @@ Compute the activity report for University of Gothenburg, for the PhD students.
 
 Author: Romain Caneill
 
-Licence:
-Copyright © 2021 Romain Caneill <romain.caneill@gu.se>
-This work is free. You can redistribute it and/or modify it under the
-terms of the Do What The Fuck You Want To Public License, Version 2,
-as published by Sam Hocevar. See below for more details.
+Licence: see attached LICENSE file.
 
-       DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-  0. You just DO WHAT THE FUCK YOU WANT TO.
+Versions:
+* 2021-11-03: v0.1, initial version
+  Romain Caneill
+    
 """
 
 """
